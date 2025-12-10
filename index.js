f6165f8715161397c51a57536f81aded92d8c096
Brenton Forwood <brentonforwood@mac.attlocal.net>
2025-12-09 22:11:00 -0500

    updated server

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get("/listItems/:id", async (req, res) => {
 });
 
 // Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
